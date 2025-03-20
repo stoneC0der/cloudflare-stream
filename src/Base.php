c42a4174dd9a85c34f0fd791838a05b9039acc2b
StoneC0der <blackleeder@gmail.com>
2025-03-20 17:00:25 +0100

    [improvement] Base class

--- src/Base.php
+++ src/Base.php
@@ -3,7 +3,6 @@
 namespace StoneC0der\CloudflareStream;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\RequestOptions;
 use JsonException;
 use Psr\Http\Message\ResponseInterface;
 
@@ -16,23 +15,12 @@ class Base
 
     public function __construct()
     {
-        $this->accountId = config('cloudflare-stream.accountId');
-        $this->authKey = config('cloudflare-stream.authKey');
-        $this->authEmail = config('cloudflare-stream.authEMail');
+        $this->accountId = env('cloudflare-stream.accountId');
+        $this->authKey = env('cloudflare-stream.authKey');
+        $this->authEmail = env('cloudflare-stream.authEMail');
 
-        $this->http = new Client(['base_uri' => "https://api.cloudflare.com/client/v4/accounts/$this->accountId/stream/",
-            RequestOptions::HEADERS => [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'Tus-Resumable' => '1.0.0',
-                'Upload-Creator' => 'creator-id_'. auth()->user()->first_name
-                    ?? auth()->user()->name ?? 'N/A'
-                    .auth()->id() ?? auth()->user()->uuid ?? '',
-                'Upload-Length' => '',
-                'Upload-Metadata' => '',
-                'X-Auth-Email' => $this->authEmail,
-                'Authorization' => "Bearer $this->authKey",
-            ],
+        $this->http = new Client([
+            'base_uri' => "https://api.cloudflare.com/client/v4/accounts/$this->accountId/stream/",
         ]);
     }
 
@@ -41,7 +29,7 @@ class Base
      */
     protected function response(ResponseInterface $response): array
     {
-        if ($response->getReasonPhrase() === 'OK') {
+        if ($response->getStatusCode() === 200) {
             return json_decode((string)$response->getBody(), true, 100, JSON_THROW_ON_ERROR);
         }
         return [];
