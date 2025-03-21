921b1c20fa056f1cf981b250ed3e4753f50d0665
StoneC0der <blackleeder@gmail.com>
2025-03-21 19:13:47 +0100

    [fix] Promo Campaign
    - fix ENV value not accessible

--- src/Base.php
+++ src/Base.php
@@ -15,9 +15,9 @@ class Base
 
     public function __construct()
     {
-        $this->accountId = env('cloudflare-stream.accountId');
-        $this->authKey = env('cloudflare-stream.authKey');
-        $this->authEmail = env('cloudflare-stream.authEMail');
+        $this->accountId = config('cloudflare-stream.accountId');
+        $this->authKey = config('cloudflare-stream.authKey');
+        $this->authEmail = config('cloudflare-stream.authEMail');
 
         $this->http = new Client([
             'base_uri' => "https://api.cloudflare.com/client/v4/accounts/$this->accountId/"
